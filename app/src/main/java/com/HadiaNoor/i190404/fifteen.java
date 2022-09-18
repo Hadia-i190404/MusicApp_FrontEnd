2a5b7155eadc63bd184ea683885bffd51c21b6ee
Hadia-i190404 <anayaahmed101@gmail.com>
2022-09-18 19:39:01 +0500

    Musify App completed

---
+++ app/src/main/java/com/HadiaNoor/i190404/fifteen.java
@@ -0,0 +1,15 @@
+
+package com.HadiaNoor.i190404;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fifteen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fifteen);
+    }
+}
---
+++ app/src/main/java/com/HadiaNoor/i190404/nineteen.java
@@ -0,0 +1,14 @@
+package com.HadiaNoor.i190404;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class nineteen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nineteen);
+    }
+}
