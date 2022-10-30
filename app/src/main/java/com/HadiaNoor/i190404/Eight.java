1be6dee417b8a231f86ec97b80c2a4ef75c03e6f
Hadia-i190404 <anayaahmed101@gmail.com>
2022-10-30 17:58:15 +0500

    Musify APP

--- app/src/main/java/com/HadiaNoor/i190404/Eight.java
+++ app/src/main/java/com/HadiaNoor/i190404/Eight.java
@@ -1,14 +1,24 @@
 package com.HadiaNoor.i190404;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Eight extends AppCompatActivity {
 
+    SharedPreferences mPref;
+    SharedPreferences.Editor editmPref;
+    ImageView left;
+    DrawerLayout drwr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eight);
+
+
     }
+
 }
